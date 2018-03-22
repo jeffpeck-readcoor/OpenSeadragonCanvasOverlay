840a3c6033edffba854c29ccb4073c6b13a29186
Jeff Peck <jeffrey.peck@readcoor.com>
2018-03-21 20:15:16 -0400

    fixed aspect ration and added preset width/height for image

--- openseadragon-canvas-overlay.js
+++ openseadragon-canvas-overlay.js
@@ -1,9 +1,9 @@
 // OpenSeadragon canvas Overlay plugin 0.0.2 based on svg overlay plugin
 
 (function() {
-    
+
     $ = window.OpenSeadragon;
-    
+
     if (!$) {
         $ = require('openseadragon');
         if (!$) {
@@ -13,7 +13,7 @@
 
     // ----------
     $.Viewer.prototype.canvasOverlay = function(options) {
-        
+
         if (this._canvasOverlayInfo) {
             return this._canvasOverlayInfo;
         }
@@ -40,24 +40,25 @@
 
         this._canvas = document.createElement('canvas');
         this._canvasdiv.appendChild(this._canvas);
-        
+
         this.onRedraw = options.onRedraw || function(){};
         this.clearBeforeRedraw = (typeof (options.clearBeforeRedraw) !== "undefined") ?
                         options.clearBeforeRedraw : true;
-                        
-                      
-        
-        this._viewer.addHandler('update-viewport', function() {         
+
+        this.presetImageWidth = options.presetImageWidth;
+        this.presetImageHeight = options.presetImageHeight;
+
+        this._viewer.addHandler('update-viewport', function() {
             self.resize();
             self._updateCanvas();
         });
 
         this._viewer.addHandler('open', function() {
             self.resize();
-            self._updateCanvas();     
+            self._updateCanvas();
         });
 
-       
+
     };
 
     // ----------
@@ -72,10 +73,22 @@
         },
         // ----------
         clear: function() {
-            this._canvas.getContext('2d').clearRect(0, 0, this._containerWidth, this._containerHeight); 
+            this._canvas.getContext('2d').clearRect(0, 0, this._containerWidth, this._containerHeight);
         },
         // ----------
         resize: function() {
+            if (this.presetImageWidth && this.presetImageHeight) {
+                this.imgWidth = this.presetImageWidth;
+                this.imgHeight = this.presetImageHeight;
+            }
+            else {
+                var image1 = this._viewer.world.getItemAt(0);
+                this.imgWidth = image1.source.dimensions.x;
+                this.imgHeight = image1.source.dimensions.y;
+            }
+
+            this.imgAspectRatio = this.imgWidth / this.imgHeight;
+
             if (this._containerWidth !== this._viewer.container.clientWidth) {
                 this._containerWidth = this._viewer.container.clientWidth;
                 this._canvasdiv.setAttribute('width', this._containerWidth);
@@ -91,26 +104,22 @@
             var boundsRect = this._viewer.viewport.getBounds(true);
             this._viewportOrigin.x = boundsRect.x;
             this._viewportOrigin.y = boundsRect.y * this.imgAspectRatio;
-            
+
             this._viewportWidth = boundsRect.width;
             this._viewportHeight = boundsRect.height * this.imgAspectRatio;
-            var image1 = this._viewer.world.getItemAt(0);
-            this.imgWidth = image1.source.dimensions.x;
-            this.imgHeight = image1.source.dimensions.y;
-            this.imgAspectRatio = this.imgWidth / this.imgHeight;
         },
         _updateCanvas: function() {
             var viewportZoom = this._viewer.viewport.getZoom(true);
             var image1 = this._viewer.world.getItemAt(0);
             var zoom = image1.viewportToImageZoom(viewportZoom);
-            
+
             var x=((this._viewportOrigin.x/this.imgWidth-this._viewportOrigin.x )/this._viewportWidth)*this._containerWidth;
             var y=((this._viewportOrigin.y/this.imgHeight-this._viewportOrigin.y )/this._viewportHeight)*this._containerHeight;
-            
+
             if (this.clearBeforeRedraw) this.clear();
             this._canvas.getContext('2d').translate(x,y);
-            this._canvas.getContext('2d').scale(zoom,zoom);     
-            this.onRedraw();    
+            this._canvas.getContext('2d').scale(zoom,zoom);
+            this.onRedraw();
             this._canvas.getContext('2d').setTransform(1, 0, 0, 1, 0, 0);
         }
     };
